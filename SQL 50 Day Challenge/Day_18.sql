9341b0f3f64f4a2d1d7458f4d409bfd5e3252c78
Amit Devkatte <amitdevkatte.ml@gmail.com>
2024-12-19 07:35:05 +0530

    Add files via upload

---
+++ SQL 50 Day Challenge/Day_18.sql
@@ -0,0 +1,91 @@
+--Day 18
+
+/*
+Write a SQL query to find out each hotel best performing months based on revenue 
+*/
+--group by horel, months
+-- then rank each month revenue
+
+
+-- Creating the hotel_bookings table
+CREATE TABLE hotel_bookings (
+    booking_id SERIAL PRIMARY KEY,
+    booking_date DATE,
+    hotel_name VARCHAR(100),
+    total_guests INT,
+    total_nights INT,
+    total_price DECIMAL(10, 2)
+);
+
+-- Inserting sample data for hotel bookings for 2023 and 2022
+INSERT INTO hotel_bookings (booking_date, hotel_name, total_guests, total_nights, total_price) VALUES
+    ('2023-01-05', 'Hotel A', 2, 3, 300.00),
+    ('2023-02-10', 'Hotel B', 3, 5, 600.00),
+    ('2023-03-15', 'Hotel A', 4, 2, 400.00),
+    ('2023-04-20', 'Hotel B', 2, 4, 500.00),
+    ('2023-05-25', 'Hotel A', 3, 3, 450.00),
+    ('2023-06-30', 'Hotel B', 5, 2, 350.00),
+    ('2023-07-05', 'Hotel A', 2, 5, 550.00),
+    ('2023-08-10', 'Hotel B', 3, 3, 450.00),
+    ('2023-09-15', 'Hotel A', 4, 4, 500.00),
+    ('2023-10-20', 'Hotel B', 2, 3, 300.00),
+    ('2023-11-25', 'Hotel A', 3, 2, 350.00),
+    ('2023-12-30', 'Hotel B', 5, 4, 600.00),
+    ('2022-01-05', 'Hotel A', 2, 3, 300.00),
+    ('2022-02-10', 'Hotel B', 3, 5, 600.00),
+    ('2022-03-15', 'Hotel A', 4, 2, 400.00),
+    ('2022-04-20', 'Hotel B', 2, 4, 500.00),
+    ('2022-05-25', 'Hotel A', 3, 3, 450.00),
+    ('2022-06-30', 'Hotel B', 5, 2, 350.00),
+    ('2022-07-05', 'Hotel A', 2, 5, 550.00),
+    ('2022-08-10', 'Hotel B', 3, 3, 450.00),
+    ('2022-09-15', 'Hotel A', 4, 4, 500.00),
+    ('2022-10-20', 'Hotel B', 2, 3, 300.00),
+    ('2022-11-25', 'Hotel A', 3, 2, 350.00),
+    ('2022-12-30', 'Hotel B', 5, 4, 600.00);
+
+select hotel_name, month_date, revenue
+FROM(
+	select hotel_name
+		,date_trunc('month',booking_date)::date month_date
+		,sum(total_price) revenue
+		,RANK() over(partition by hotel_name order by sum(total_price) desc) rnk
+	from hotel_bookings
+	group by hotel_name,month_date
+)
+where rnk=1
+order by hotel_name, month_date;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
